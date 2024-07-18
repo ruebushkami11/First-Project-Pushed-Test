5ab6e506e9a3e01ae82784a562e14a5b801897fb
Kami Ruebush <kami691@revature.net>
2024-07-18 13:30:37 -0600

    Kami made updates and pushed code

---
+++ Read.md
@@ -0,0 +1,3 @@
+This project was created for practicing Github uploading by Kami Ruebush
+
+/<h2> This was created on 7/18/24
--- src/Github.java
+++ src/Github.java
@@ -4,7 +4,8 @@ public class Github {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
-
+		System.out.println("Hello Github I am adding and reuploading");
+		
 	}
 
 }
